5cd7077a44e3c64a4982b69441c410f3cafe4e85
Edgar <edgar.villasenor@wsu.edu>
2020-10-01 12:10:32 -0700

    Start lab 3

---
+++ Lab 3/lab/main.c
@@ -0,0 +1,13 @@
+/* Lab 3 SH Simulator */
+
+int main(int argc, char *argv[], char *env[]){
+    
+    printf("Entered sh simulator\n");
+
+    int status = 0;
+    while(status != -1){
+        status = -1;
+    }
+    
+    return (0);
+}
