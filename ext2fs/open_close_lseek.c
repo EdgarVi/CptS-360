d35de9b7b71789c68453564e978ccdd9f2e19c4a
Edgar <edgar.villasenor@wsu.edu>
2020-12-01 12:56:07 -0800

    Add mode type

--- ext2fs/open_close_lseek.c
+++ ext2fs/open_close_lseek.c
@@ -1,5 +1,20 @@
 #include "symlink.c"
 
-int my_open(char * path, char * mode) {
-    printf("%s %s", path, mode);
+int my_open(char * path, char * mode_input) {
+    int mode, device, i_number, device = running->cwd->dev, i, fd;
+    MINODE * mip;
+    OFT *oftp;
+
+    // set mode type
+    if(strcmp(mode_input, "R") == 0)
+        mode = READ_TYPE;
+    if(strcmp(mode_input, "W") == 0)
+        mode = WRITE_TYPE;
+    if(strcmp(mode_input, "RW") == 0)
+        mode = READ_WRITE_TYPE;
+    if(strcmp(mode_input, "APPEND") == 0)
+        mode = APPEND_TYPE;
+
+    
+        
 }
