04af3d048b96d8f093a43033b2c6c726eec87e1d
Edgar <edgar.villasenor@wsu.edu>
2020-12-01 14:28:19 -0800

    Add open

--- ext2fs/open_close_lseek.c
+++ ext2fs/open_close_lseek.c
@@ -1,5 +1,52 @@
 #include "symlink.c"
 
+int open_file(MINODE * mip, int mode){
+
+    OFT *ofp;
+	int i;
+
+	if(duplicate_oft(mip) == 1)
+	{
+		printf("Error: File already opened for something other than READ\n");
+		return -1;
+	}
+
+	ofp = get_oft();
+	
+
+	ofp->mode = mode;
+	ofp->minodePtr = mip;
+	
+	switch(ofp->mode)
+	{
+		case READ_TYPE: 
+            ofp->offset = 0;
+		    break;
+		case WRITE_TYPE: 
+            clear_blocks(mip);
+		    break;
+		case READ_WRITE_TYPE: 
+            ofp->offset = 0;
+		    break;
+		case APPEND_TYPE: 
+            ofp->offset = mip->ip.i_size;
+		    break;
+	}
+
+	for(i = 0; i < NFD; i++)
+	{
+		if(running->fd[i] == NULL)
+		{
+			running->fd[i] = ofp;
+			return i;
+		}
+	}
+	
+    printf("Panic: Out of proc oft pointers\n");
+	return -1;
+
+}
+
 int my_open(char * path, char * mode_input) {
     int mode, device, i_number, device = running->cwd->dev, i, fd;
     MINODE * mip;
@@ -15,6 +62,24 @@ int my_open(char * path, char * mode_input) {
     if(strcmp(mode_input, "APPEND") == 0)
         mode = APPEND_TYPE;
 
+    i_number = getino(device, path);
+
+    // error checking
+    if(i_number < 1) {
+        printf("Error: file does not exist\n");
+        return -1;
+    }
+
+    mip = iget(device, i_number);
+
+    if(!S_ISREG(mip->ip.i_mode)) {
+        printf("Error: file is not regular\n");
+        return -1;
+    }
+
+    // passed checks now open file
+    fd = open_file(mip, mode);
     
+    return fd;
         
 }
