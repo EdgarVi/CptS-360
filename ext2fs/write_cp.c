4ed82840ff7b5e61db43ff5dde5f218b5d64f60c
Edgar <edgar.villasenor@wsu.edu>
2020-12-08 16:28:29 -0800

    Add my_write

--- ext2fs/write_cp.c
+++ ext2fs/write_cp.c
@@ -2,7 +2,123 @@
 
 
 int my_write(int dest_fd, char * buf, int num_bytes) {
-    printf("called my write\n");
+    
+	int dev, count = 0, remaining, logical_block, physical_block, double_block, start_byte;
+	int ind_index, double_index, blk;
+	int size;
+	char *src, *dest;
+	int *indirect, *double_indirect;
+	char local_buf[BLKSIZE];
+	OFT *ofp;
+	char *cp, *buf_cp;
+	MINODE *mip;
+	INODE *ip;
+
+	ofp = running->fd[fd];
+	dev = ofp->minodePtr->dev;
+	mip = ofp->minodePtr;
+	ip = &ofp->minodePtr->ip;
+	buf_cp = buf;
+
+	while(num_bytes)
+	{
+		logical_block = ofp->offset / BLKSIZE;
+		start_byte = ofp->offset % BLKSIZE;
+
+		//direct
+		if(logical_block < NUM_DIRECT_BLOCKS)
+		{
+			if(ip->i_block[logical_block] == 0)
+			{
+				mip->ip.i_block[logical_block] = allocate_block(mip->dev);
+			}
+			physical_block = ip->i_block[logical_block];
+		}
+		//indirect
+		else if(logical_block >= NUM_DIRECT_BLOCKS && 
+			logical_block < NUM_DIRECT_BLOCKS + BLOCK_NUMBERS_PER_BLOCK)
+		{
+
+			if(ip->i_block[NUM_INDIRECT_BLOCKS] == 0)
+			{
+				mip->ip.i_block[logical_block] = ialloc(mip->dev);
+			}
+			get_block(dev, ip->i_block[NUM_INDIRECT_BLOCKS], local_buf);
+			indirect = (int *)local_buf;
+
+			if(indirect[logical_block - NUM_DIRECT_BLOCKS] == 0)
+			{
+				indirect[logical_block - NUM_DIRECT_BLOCKS] = allocate_block(mip->dev);
+				put_block(mip->dev, ip->i_block[NUM_INDIRECT_BLOCKS], local_buf);
+			}
+
+			physical_block = indirect[logical_block - NUM_DIRECT_BLOCKS];
+		}
+		//double indirect
+		else
+		{
+			if(ip->i_block[NUM_DOUBLE_INDIRECT_BLOCKS] == 0)
+			{
+				ip->i_block[NUM_DOUBLE_INDIRECT_BLOCKS] = allocate_block(mip->dev);
+			}
+			double_index = (logical_block - (NUM_DIRECT_BLOCKS + BLOCK_NUMBERS_PER_BLOCK)) / BLOCK_NUMBERS_PER_BLOCK;
+			ind_index = (logical_block - (NUM_DIRECT_BLOCKS + BLOCK_NUMBERS_PER_BLOCK)) % BLOCK_NUMBERS_PER_BLOCK;
+
+			get_block(dev, ip->i_block[NUM_DOUBLE_INDIRECT_BLOCKS], local_buf);
+			double_indirect = (int *)local_buf;
+
+			physical_block = double_indirect[double_index];
+
+			if(physical_block == 0)
+			{
+				double_indirect[double_index] = allocate_block(mip->dev);
+				physical_block = double_indirect[double_index];
+				put_block(mip->dev, ip->i_block[NUM_DOUBLE_INDIRECT_BLOCKS], local_buf);
+			}
+
+			get_block(dev, physical_block, local_buf);
+			double_block = physical_block;
+
+			indirect = (int *)local_buf;
+
+			physical_block = indirect[ind_index];
+
+			if(physical_block == 0)
+			{
+				indirect[ind_index] = allocate_block(mip->dev);
+				physical_block = indirect[ind_index];
+				put_block(mip->dev, double_block, local_buf);
+			}
+		}
+
+		get_block(dev, physical_block, local_buf);
+
+		cp = local_buf + start_byte;
+		remaining = BLKSIZE - start_byte;
+
+		if(remaining <= num_bytes)
+		{
+			size = remaining;
+		}
+		else if(num_bytes <= remaining)
+		{
+			size = num_bytes;
+		}
+
+			
+		memcpy(cp, buf, size);
+		ofp->offset += size;
+		if(ofp->offset > ofp->minodePtr->ip.i_size)
+		{
+			mip->ip.i_size += size;
+		}
+		count += size;
+		num_bytes -= size;
+		remaining -= size;
+		put_block(dev, physical_block, local_buf);
+	}
+	mip->dirty = 1;
+	return count;
 }
 
 // implementation of command "cp" to copy contents of one file to another
